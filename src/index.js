249a90e4dee654bf42baa62ff79344af1b498f83
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-13 01:36:58 +0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import fs from 'fs';
+import { has } from 'lodash';
 
 export default (path1, path2) => {
-    const data1 = JSON.parse(fs.readFileSync(fs.realpathSync(path1)));
-    const data2 = JSON.parse(fs.readFileSync(fs.realpathSync(path2)));
+  const beforeData = JSON.parse(fs.readFileSync(fs.realpathSync(path1)));
+  const afterData = JSON.parse(fs.readFileSync(fs.realpathSync(path2)));
+  const keys = Array.from(new Set([...Object.keys(beforeData), ...Object.keys(afterData)]));
 
-    const result = 
+  const result = keys.reduce((acc, currentKey) => {
+    if (has(afterData, currentKey) && has(afterData, currentKey)) {
+      return (beforeData[currentKey] == afterData[currentKey])
+        ? [...acc, `  ${currentKey}: ${afterData[currentKey]}`]
+        : [...acc, `+ ${currentKey}: ${afterData[currentKey]}\n- ${currentKey}: ${beforeData[currentKey]}`];
+    }
+    if (has(afterData, currentKey)) {
+      return [...acc, `+ ${currentKey}: ${afterData[currentKey]}`];
+    }
+    return [...acc, `- ${currentKey}: ${beforeData[currentKey]}`];
+  }, []);
+  console.log(`{\n${result.join('\n')}\n}`);
+  return `{\n${result.join('\n')}\n}`;
 };
