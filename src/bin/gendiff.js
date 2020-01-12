d954127bab8885330cee57a3ed1d8455ce7fdeba
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-12 22:06:04 +0400

    update gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,4 +7,7 @@ const program = new commander.Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .action()
   .parse(process.argv);
