dba284f3b1849764cfb1645e496f04a161b3c707
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-13 02:53:20 +0400

    update gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
+import genDiff from '..';
 
 const program = new commander.Command();
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
-  .action()
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => genDiff(firstConfig, secondConfig))
   .parse(process.argv);
