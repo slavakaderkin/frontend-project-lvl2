c850bc6f7b002a1e24de883077dbc1e651b3a644
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-13 17:14:39 +0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,17 @@ export default (path1, path2) => {
   const keys = Array.from(new Set([...Object.keys(beforeData), ...Object.keys(afterData)]));
 
   const result = keys.reduce((acc, currentKey) => {
-    if (has(afterData, currentKey) && has(afterData, currentKey)) {
-      // eslint-disable-next-line eqeqeq
-      return (beforeData[currentKey] == afterData[currentKey])
+    if (has(afterData, currentKey) && has(beforeData, currentKey)) {
+      return (afterData[currentKey] === beforeData[currentKey])
         ? [...acc, `  ${currentKey}: ${afterData[currentKey]}`]
-        : [...acc, `+ ${currentKey}: ${afterData[currentKey]}\n - ${currentKey}: ${beforeData[currentKey]}`];
+        : [...acc, `+ ${currentKey}: ${afterData[currentKey]}\n- ${currentKey}: ${beforeData[currentKey]}`];
     }
     if (has(afterData, currentKey)) {
       return [...acc, `+ ${currentKey}: ${afterData[currentKey]}`];
     }
+
     return [...acc, `- ${currentKey}: ${beforeData[currentKey]}`];
   }, []);
-  return `{\n ${result.join('\n ')}\n}`;
+
+  return `{\n${result.join('\n')}\n}`;
 };
