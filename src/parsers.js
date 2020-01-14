e4f9645c73ad03733872a56e466416ffe18cd9c3
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-14 13:55:07 +0400

    add ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,13 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 export default (pathToFile) => {
   const expansion = path.extname(pathToFile);
 
   if (expansion === '.yml') {
-    return yaml.safeLoad(fs.readFileSync(fs.realpathSync(pathToFile)));
+    return yaml.safeLoad(fs.readFileSync(fs.realpathSync(pathToFile), 'utf-8'));
   }
 
-  return JSON.parse(fs.readFileSync(fs.realpathSync(pathToFile)));
+  if (expansion === '.ini') {
+    return ini.parse(fs.readFileSync(fs.realpathSync(pathToFile), 'utf-8'));
+  }
+
+  return JSON.parse(fs.readFileSync(fs.realpathSync(pathToFile), 'utf-8'));
 };
