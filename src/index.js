60c0fabb9aeedf900e63176db0e3f8fb080e3981
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-14 12:36:18 +0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import fs from 'fs';
 import { has } from 'lodash';
+import parse from './parsers';
 
 export default (path1, path2) => {
-  const beforeData = JSON.parse(fs.readFileSync(fs.realpathSync(path1)));
-  const afterData = JSON.parse(fs.readFileSync(fs.realpathSync(path2)));
+  const beforeData = parse(path1);
+  const afterData = parse(path2);
   const keys = Array.from(new Set([...Object.keys(beforeData), ...Object.keys(afterData)]));
 
   const result = keys.reduce((acc, currentKey) => {
