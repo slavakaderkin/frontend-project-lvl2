d1fad69122a818898dfcfb1b3bba021f7dffe53d
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-19 23:39:58 +0400

    update formatters/index

--- src/formaters/index.js
+++ src/formaters/index.js
@@ -4,6 +4,7 @@ import signsFormatter from './signsFormatter';
 const formatters = {
   signs: signsFormatter,
   plain: plainFormatter,
+  json: JSON.stringify,
 };
 
-export default (ast, format = 'signs') => formatters[format](ast);
+export default (ast, format = 'signs') => formatters[format](ast, null, 4);
