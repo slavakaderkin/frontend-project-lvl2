86f94b77b7306c067a94dc587bc965b2a1b1fc0a
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-20 01:40:14 +0400

    add and update tests and fixtures

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,15 +6,16 @@ const pathToJsonAfterFile = `${__dirname}/__fixtures__/after.json`;
 const pathToIniBeforeFile = `${__dirname}/__fixtures__/before.ini`;
 const pathToIniAfterFile = `${__dirname}/__fixtures__/after.ini`;
 const pathToYmlBeforeFile = `${__dirname}/__fixtures__/before.yml`;
-const pathToymlAfterFile = `${__dirname}/__fixtures__/after.yml`;
+const pathToYmlAfterFile = `${__dirname}/__fixtures__/after.yml`;
 
 const signResult = fc.readFileSync(`${__dirname}/__fixtures__/signsResult.txt`, 'utf-8');
 const plainResult = fc.readFileSync(`${__dirname}/__fixtures__/plainResult.txt`, 'utf-8');
+const jsonResult = fc.readFileSync(`${__dirname}/__fixtures__/jsonResult.txt`, 'utf-8');
 
 test.each([
   [pathToJsonBeforeFile, pathToJsonAfterFile, signResult],
   [pathToIniBeforeFile, pathToIniAfterFile, signResult],
-  [pathToYmlBeforeFile, pathToymlAfterFile, signResult],
+  [pathToYmlBeforeFile, pathToYmlAfterFile, signResult],
 ])('test genDiff signs format', (a, b, expected) => {
   const actual = genDiff(a, b);
 
@@ -24,9 +25,19 @@ test.each([
 test.each([
   [pathToJsonBeforeFile, pathToJsonAfterFile, plainResult],
   [pathToIniBeforeFile, pathToIniAfterFile, plainResult],
-  [pathToYmlBeforeFile, pathToymlAfterFile, plainResult],
+  [pathToYmlBeforeFile, pathToYmlAfterFile, plainResult],
 ])('test genDiff plains format', (a, b, expected) => {
   const actual = genDiff(a, b, 'plain');
+
+  expect(actual).toBe(expected);
+});
+
+test.each([
+  [pathToJsonBeforeFile, pathToJsonAfterFile, jsonResult],
+  [pathToIniBeforeFile, pathToIniAfterFile, jsonResult],
+  [pathToYmlBeforeFile, pathToYmlAfterFile, jsonResult],
+])('test genDiff json format', (a, b, expected) => {
+  const actual = genDiff(a, b, 'json');
 
   expect(actual).toBe(expected);
 });
