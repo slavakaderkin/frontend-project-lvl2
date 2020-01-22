920df3236fb668402b4f87f398d66396fd4e5b72
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-22 13:39:48 +0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+import fs from 'fs';
+import path from 'path';
 import parse from './parsers';
 import genAst from './genAts';
 import render from './formatters';
 
+const getData = (pathToFile) => fs.readFileSync(fs.realpathSync(pathToFile), 'utf-8');
+const getType = (pathToFile) => {
+  const expansion = path.extname(pathToFile);
+  return expansion.slice(1);
+};
+
 export default (pathToBeforeFile, pathToAfterFile, format) => {
-  const beforeData = parse(pathToBeforeFile);
-  const afterData = parse(pathToAfterFile);
+  const typeBeforeFile = getType(pathToBeforeFile);
+  const typeAfterFile = getType(pathToAfterFile);
+
+  const beforeData = parse(getData(pathToBeforeFile), typeBeforeFile);
+  const afterData = parse(getData(pathToAfterFile), typeAfterFile);
 
   return render(genAst(beforeData, afterData), format);
 };
