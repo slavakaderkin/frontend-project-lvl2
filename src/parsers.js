495d89e99d33e00199723e0690640fd36b014bbf
Slava Kaderkin <slavakaderkin@gmail.com>
2020-01-22 13:39:18 +0400

    update parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,22 +1,10 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parserJson = (pathToFile) => JSON.parse(fs.readFileSync(fs.realpathSync(pathToFile), 'utf-8'));
-const parserYml = (pathToFile) => yaml.safeLoad(fs.readFileSync(fs.realpathSync(pathToFile), 'utf-8'));
-const parserIni = (pathToFile) => ini.parse(fs.readFileSync(fs.realpathSync(pathToFile), 'utf-8'));
-
-export default (pathToFile) => {
-  const expansion = path.extname(pathToFile);
-
-  if (expansion === '.yml') {
-    return parserYml(pathToFile);
-  }
-
-  if (expansion === '.ini') {
-    return parserIni(pathToFile);
-  }
-
-  return parserJson(pathToFile);
+const parsers = {
+  json: JSON.parse,
+  yml: yaml.safeLoad,
+  ini: ini.parse,
 };
+
+export default (data, type) => parsers[type](data);
